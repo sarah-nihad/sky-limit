f4636d7dc55fceb7fedce1ea22f25505c56c37d1
sarah-nihad <50545412+sarah-nihad@users.noreply.github.com>
2019-08-17 15:32:48 +0300

    s

--- src/component/Pricing.js
+++ src/component/Pricing.js
@@ -17,17 +17,17 @@ class Pricing extends Component {
  <Row style={{ marginRight: '0px', width: '90%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
     <Col lg={4} id='colomnservices1'   >
     <p style={{ marginTop: '5px',color:'purple',fontSize:'26px',fontWeight:'600' }}  >Vision</p>
-    <div id='boxsss'>
-                            <div id='cont'>
+    <div className='card'>
+                           
 {/* <div  id='divcolss' > */}
-<div>
+<div  className='details'>
         <p style={{ marginTop: '5px',color:'#fff',fontSize:'26px',fontWeight:'600' }}  >Vision</p>
         <div  style={{padding:'14px'}} id='concepttext' >collaborate to get more vision wideness</div>
         </div>
-<div> <img src={require('../img/vision.jpg')}  alt='img' />   </div>
+<div className='imgBox'> <img src={require('../img/vision.jpg')}  alt='img' />   </div>
 
 {/* </div> */}
-</div></div>
+</div>
 
     </Col>
 
@@ -35,29 +35,29 @@ class Pricing extends Component {
 
 {/* <div id='divcolss'> */}
  <p style={{ marginTop: '5px',color:'purple',fontSize:'26px',fontWeight:'600' }}  >Customize</p>
- <div id='boxsss'>
-                            <div id='cont'>
-<div> <img src={require('../img/customize.jpg')}  alt='img' />   </div>
+ <div className='card'>
+                           
+<div className='imgBox' > <img src={require('../img/customize.jpg')}  alt='img' />   </div>
 {/* <div> <img src={require('../img/feedback.png')} style={{ height: '40px' }} alt='img' />   </div> */}
-<div>
+<div  className='details' >
    <p style={{ marginTop: '5px',color:'#fff',fontSize:'26px',fontWeight:'600' }}  >Customize</p>
    <div style={{padding:'10px'}} id='concepttext' >We can add or customized any requirements to help your organization</div>
  </div> 
- </div></div>
+ </div>
 </Col>
 
 
     <Col lg={4} id='colomnservices12' >
     {/* <div id='divcolss'  > */}
     <p style={{ marginTop: '5px',color:'purple',fontSize:'26px',fontWeight:'600' }} >Advisory</p>
-    <div id='boxsss'>
-                            <div id='cont'>
-    <div> <img src={require('../img/Advisory.jpg')}  alt='img' />   </div>
-    <div>
+    <div className='card'>
+                           
+    <div className='imgBox'> <img src={require('../img/Advisory.jpg')}  alt='img' />   </div>
+    <div className='details' >
         <p style={{ marginTop: '5px',color:'#fff',fontSize:'26px',fontWeight:'600' }} >Advisory</p>
         <div  style={{padding:'10px'}} id='concepttext' > monthly meeting to discuss
         results and to increase efficiency</div>
-        </div> </div></div>
+        </div> </div>
     </Col>
 
 
@@ -70,42 +70,42 @@ class Pricing extends Component {
 
     <Col lg={4} id='colomnservices1'>
     <p style={{ marginTop: '5px',color:'purple',fontSize:'26px',fontWeight:'600' }} >Customer Service</p>
-    {/* <div  id='divcolss'> */}  <div id='boxsss'>
-                            <div id='cont'>
-    <div> <img src={require('../img/Coustomer service.jpg')}  alt='img' />   </div>
-   <div>
+    {/* <div  id='divcolss'> */}  <div className='card'>
+                           
+    <div className='imgBox'> <img src={require('../img/Coustomer service.jpg')}  alt='img' />   </div>
+   <div  className='details' >
         <p style={{ marginTop: '5px',color:'#fff',fontSize:'26px',fontWeight:'600' }} >Customer Service</p>
         <div  style={{padding:'13px'}} id='concepttext' >phone and email</div>
         <p></p>
         <p></p>
-    </div></div></div>
+    </div></div>
     </Col> 
     <Col lg={4} id='colomnservices1'>
      {/* <div id='divcolss'  > */}
      <p style={{ marginTop: '5px',color:'purple',fontSize:'26px',fontWeight:'600' }} >Training</p>
-     <div id='boxsss'>
-                            <div id='cont'>
+     <div className='card'>
+                            
      {/* <div> <img src={require('../img/career.png')} style={{ height: '40px' }} alt='img' />   </div> */}
-     <div> <img src={require('../img/training.jpg')} alt='img' />   </div>
-     <div>
+     <div className='imgBox'> <img src={require('../img/training.jpg')} alt='img' />   </div>
+     <div  className='details'>
         <p style={{ marginTop: '5px',color:'#fff',fontSize:'26px',fontWeight:'600' }} >Training</p>
         <div  style={{padding:'13px'}} id='concepttext'  >Free Training to the new users</div>
         <p>  </p>
-       </div></div>
+       </div>
      </div>
     </Col>
     <Col lg={4} id='colomnservices12' >
     {/* <div id='divcolss'  > */}
     <p style={{ marginTop: '5px',color:'purple',fontSize:'26px',fontWeight:'600' }} >Update</p>
-    <div id='boxsss'>
-                            <div id='cont'>
-    <div> <img src={require('../img/update.jpg')}  alt='img' />   </div>
+    <div className='card'>
+                            
+    <div className='imgBox'> <img src={require('../img/update.jpg')}  alt='img' />   </div>
      {/* <div> <img src={require('../img/update.png')} style={{ height: '40px' }} alt='img' />   </div> */}
-     <div>
+     <div  className='details'>
         <p style={{ marginTop: '5px',color:'#fff',fontSize:'26px',fontWeight:'600' }} >Update</p>
         <div  style={{padding:'13px'}} id='concepttext' >Updating data on weekly basis</div>
         <p></p>
-    </div></div></div>
+    </div></div>
     </Col>
 
 
---
+++ src/component/Test.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import './nav1.css';
+class Test extends Component{
+    render(){
+        return(
+            <div>
+                <div className='card'>
+                    <div className='imgBox'>
+                        <img src={require('../img/update.jpg')} style={{width:'100%'}} />
+                    </div>
+                    <div className='details'>
+                        <h4>sarah nihad hhhh</h4>
+                        <p> dd ffffffffff jhgt fffff yyyyyy sssssssss</p>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+export default Test;
